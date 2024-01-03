535b9d6578e18ca5b1ee72e467c95d557a3c470f
sergioMG-Unir <sergio.minguez178@comunidadunir.net>
2024-01-03 18:44:37 +0100

    Modificadas las cards, añadidas varias funciones, modificada navbar, añadidas más páginas

---
+++ TODO.md
@@ -0,0 +1,7 @@
+▸	Cuatro componentes, incluyendo Data-Binding en cada uno. 
+▸	La aplicación tiene que ser responsiva, por lo que tendremos que aplicar Bootstrap (Angular Material o cualquier otro para los estilos).
+▸	Incluiremos directivas para la gestión del HTML en los componentes creados. 
+
+* La aplicación contiene 4 componentes incluyendo Data Binding.
+* La aplicación incluye directivas.
+* La aplicación es responsiva y utiliza Bootstrap.
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,14 +6,49 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+  numberCard = 1;
+  nameCard = 'Título';
+  descriptionCard = 'Descripción';
+  imagePath = "../../assets/example.png";
+  numberItems = Array.from({ length: 3 }, (_, index) => index + 1);
+  buttonName ="Ver más noticias";
+  stateHidden = false;
 
   constructor(private route: Router) { }
 
-  ngOnInit() {
+  loadMoreCards($event) {
+    if (this.numberItems.length < 9) {
+      for (let i = 0; i < 3; i++) {
+        this.numberItems.push(this.numberItems.length + 3);
+      }
+  
+      if (this.numberItems.length === 9) {
+        this.buttonName = "Ver Todas";
+        this.stateHidden = true;
+        console.log( this.stateHidden);
+
+      }
+    } else if (this.numberItems.length >= 9 && this.numberItems.length <= 12) {
+      this.route.navigate(['/news']);
+      return; 
+    }
+
   }
 
-  onLoadServers(id: number){
-    this.route.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: 1}, fragment: 'loading'}, )
+  seeAllNews($event) {
+    this.route.navigate(['/news']);
+    return; 
+  }
+
+  ngOnInit() {
+    console.log("Refrescado");
+    console.log( this.stateHidden);
+
+  }
+
+  onLoadServers(id: number) {
+    this.route.navigate(['/servers', id, 'edit'], { queryParams: { allowEdit: 1 }, fragment: 'loading' });
   }
 }
